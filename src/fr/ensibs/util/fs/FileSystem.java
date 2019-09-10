4ff10c5fc49d32919522130fff25e3e62a61dce3
totatoto <adam.bernouy@orange.fr>
2019-09-10 12:30:18 +0200

    javadoc filesystem

--- src/fr/ensibs/util/fs/FileSystem.java
+++ src/fr/ensibs/util/fs/FileSystem.java
@@ -2,7 +2,16 @@ package fr.ensibs.util.fs;
 
 import java.io.InputStream;
 
-
+/**
+ * Representation of the device file system
+ */
 public interface FileSystem {
+	
+	/**
+	 * Open a file from a path into an InputStream
+	 * @param path The path of the file to open
+	 * @return The InputStream generated from the filepaths
+	 * @pre path != null && path.length > 0
+	 */
 	InputStream openFile(String path);
 }
