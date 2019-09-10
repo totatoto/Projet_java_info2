8023bf4ea228ea12f10dbbc913020ed749467bf2
chevreto <28733371+chevreto@users.noreply.github.com>
2019-09-10 13:23:10 +0200

    doc fs

--- src/fr/ensibs/util/fs/FileSystem.java
+++ src/fr/ensibs/util/fs/FileSystem.java
@@ -8,10 +8,11 @@ import java.io.InputStream;
 public interface FileSystem {
 	
 	/**
-	 * Open a file from a path into an InputStream
+	 * Open a file from a path into an InputStream, if the file cannot be found in the file system, it will check for
+	 * a file in the resources bundle in the jar. If nothing can be found, null is returned
 	 * @param path The path of the file to open
-	 * @return The InputStream generated from the filepaths
-	 * @pre path != null && path.length > 0
+	 * @return The InputStream generated from the filepath or null if no file is found
+	 * @pre path != null && path.length() > 0
 	 */
 	InputStream openFile(String path);
 }
