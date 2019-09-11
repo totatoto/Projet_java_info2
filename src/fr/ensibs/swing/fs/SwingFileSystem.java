41146852a61e3a46f81f9c9620d69a4b7fc54a01
chevreto <daphnis.chevreton@wanadoo.fr>
2019-09-11 13:32:27 +0200

    documentation partie 3

--- src/fr/ensibs/swing/fs/SwingFileSystem.java
+++ src/fr/ensibs/swing/fs/SwingFileSystem.java
@@ -4,19 +4,34 @@ import fr.ensibs.util.fs.FileSystem;
 
 import java.io.*;
 
+/**
+ * Implementation of the file system for swing use
+ * @author Daphnis Chevreton
+ */
 public class SwingFileSystem implements FileSystem {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public InputStream openFile(String path) {
+
+        //Input stream that will be returned if there is no error
         InputStream res = null;
+
+        //Check if path is not null or empty
         if(path != null && path.length() > 0){
+
+            //Create a file from the path and check if the file exists on the disk
             File file = new File(path);
             if(file.exists()){
                 try {
+                    //If the file exists, we try to open an input stream on the file and we return it
                     res = new BufferedInputStream(new FileInputStream(file));
                 } catch (FileNotFoundException ignored) {
                 }
             }else{
+                //if the file does not exists on the disk, we try looking for it in the resources of the program
                 res = getClass().getClassLoader().getResourceAsStream(path);
             }
 
--- src/fr/ensibs/swing/sprites/SwingMain.java
+++ src/fr/ensibs/swing/sprites/SwingMain.java
@@ -4,16 +4,24 @@ import fr.ensibs.graphic.Graphic;
 import fr.ensibs.graphic.ImageLoader;
 import fr.ensibs.sprites.engines.Controller;
 import fr.ensibs.swing.fs.SwingFileSystem;
-import fr.ensibs.swing.graphic.SwingGraphic;
+import fr.ensibs.swing.graphic.SwingGraphicEmpty;
 import fr.ensibs.swing.graphic.SwingImageLoader;
 import fr.ensibs.util.fs.FileSystem;
 
 import javax.swing.*;
 
+/**
+ * This class is the main entry point of the program and use the swing implementation of the different interfaces
+ * @author Daphnis Chevreton
+ */
 public class SwingMain {
 
+    /**
+     * Main method
+     * @param args arguments of the program
+     */
     public static void main(String[] args) {
-        Graphic graphic = new SwingGraphic(new JLabel());
+        Graphic graphic = new SwingGraphicEmpty(new JLabel());
         FileSystem system = new SwingFileSystem();
         ImageLoader loader = new SwingImageLoader();
 
