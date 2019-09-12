5c06a869ee450022031a281c01e63a5850054b6d
chevreto <daphnis.chevreton@wanadoo.fr>
2019-09-12 10:01:46 +0200

    implementation paintComponent

--- src/fr/ensibs/swing/graphic/SwingGraphic.java
+++ src/fr/ensibs/swing/graphic/SwingGraphic.java
@@ -4,10 +4,12 @@ import fr.ensibs.graphic.Graphic;
 import fr.ensibs.graphic.Image;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Objects;
 
 /**
  * Swing implementation of the graphic interface
+ *
  * @author Daphnis Chevreton
  */
 public class SwingGraphic implements Graphic<SwingImage> {
@@ -17,13 +19,19 @@ public class SwingGraphic implements Graphic<SwingImage> {
      */
     private JComponent component;
 
+
+    private ImageDisplay display;
+
     /**
      * Create a new swing
+     *
      * @param component the component that will hold the image
      * @throws NullPointerException if the component is null
      */
-    public SwingGraphic(JComponent component){
+    public SwingGraphic(JComponent component) {
         this.component = Objects.requireNonNull(component);
+        display = new ImageDisplay();
+        this.component.add(display);
     }
 
     /**
@@ -31,6 +39,20 @@ public class SwingGraphic implements Graphic<SwingImage> {
      */
     @Override
     public void display(SwingImage image) {
-        this.component.add(new JLabel(new ImageIcon(((SwingImage)image).getImage())));
+        display.setPreferredSize(image.getDimension());
+        display.current = image;
+        display.repaint();
     }
+
+    private static class ImageDisplay extends JComponent {
+
+        private SwingImage current;
+
+        @Override
+        public void paintComponent(Graphics g) {
+            if (current != null)
+                g.drawImage(current.getImage(), 0, 0, current.getWidth(), current.getHeight(), null);
+        }
+    }
+
 }
